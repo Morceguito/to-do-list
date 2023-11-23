456f8e4c2fe0aea47f69ab634b223f2e99d9fac7
Daniel Baptista <danielbaptista98@gmail.com>
2023-11-23 23:34:59 +0000

    First commit

---
+++ src/test/java/com/morceguito/training/todolistapp/TodolistappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morceguito.training.todolistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
